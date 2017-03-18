ac2d944da5151a1ae9b9b7a667cdb38477e29bb0
lsorber <laurent.sorber@gmail.com>
2017-03-18 12:09:19 +0100

    Improve paths. Better configure target. Parallel make.

--- bld.bat
+++ bld.bat
@@ -6,22 +6,23 @@ SET CXX=g++
 REM SET WITH_BLAS_LIB="-L%PREFIX%/lib -lblas"
 REM SET WITH_LAPACK_LIB="-L%PREFIX%/lib -llapack"
 
-SET WITH_BLAS_LIB="-L%PREFIX%\Library\bin -lmkl_rt -lpthread -lm -ldl"
-SET WITH_LAPACK_LIB="-L%PREFIX%\Library\bin -lmkl_rt -lpthread -lm -ldl"
-SET WITH_BLAS_LIB=%WITH_BLAS_LIB:\=/%
-SET WITH_LAPACK_LIB=%WITH_LAPACK_LIB:\=/%
+SET PREFIX=%PREFIX:\=/%
+SET WITH_BLAS_LIB="-L%PREFIX%/Library/bin -Wl,--no-as-needed -lmkl_rt -lpthread -lm"
+SET WITH_LAPACK_LIB="-L%PREFIX%/Library/bin -Wl,--no-as-needed -lmkl_rt -lpthread -lm"
 
 ls
 
-bash configure  ^
-  --build=x86_64-pc-winnt ^
+bash configure ^
+  --build=x86_64-w64-mingw32 ^
+  --host=x86_64-w64-mingw32 ^
+  --target=x86_64-w64-mingw32 ^
   --prefix="%PREFIX%" ^
   --exec-prefix="%PREFIX%" ^
   --with-blas-lib=%WITH_BLAS_LIB% ^
   --with-lapack-lib=%WITH_LAPACK_LIB% ^
   --enable-cbc-parallel
 
-make
+make -j
 
 REM if [ "${UNAME}" == "Linux" ]; then
 REM  make test
